4f7398f77b2f0c12a8583f24a7735443d00ddba5
makidaiki9180@gmail.com <makidaiki9180@gmail.com>
2023-05-14 17:28:21 +0800

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,5 +4,6 @@
   </x-slot>
 コンポーネントテスト1
 
-  <x-tests.card title="タイトル1" content="本文1" :message="$message"></x-tests.card>
+  <x-tests.card title="タイトル1" content="本文1" :message="$message" />
+  <x-tests.card title="タイトル2" />
 </x-tests.app>
